b26d3ad80ab084c3e6ba18354b34c5dc08b82835
Ram Ganesan <1862419+janakg@users.noreply.github.com>
2021-05-30 11:18:45 +0530

    password json file

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ az group create --name hs-es-v1 --location northeurope
 az deployment group create \
   --resource-group hs-es-v1 \
   --template-uri https://raw.githubusercontent.com/hellosivi/azure-marketplace/master/src/mainTemplate.json \
-  --parameters @parameters/secrets.password.parameters.json
+  --parameters @parameters/secrets.password.parameters.json \
+  --debug
